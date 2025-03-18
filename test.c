e01778d42cdb54c78c6b13ee8fe5aced5ba86e3e
Wei-Cheng Chen <3331363@gmail.com>
2025-03-18 17:00:12 +0800

    2025.03.18

--- test.c
+++ test.c
@@ -3,28 +3,11 @@
 #include <string.h>
 
 
-int removeElement(int* nums, int numsSize, int val) {
-    int left = 0;
-    for (int right = 0; right < numsSize; right++) {
-        if (nums[right] != val) {
-            nums[left] = nums[right];
-            left++;
-        }
-    }
-    return left;  
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    
 }
 
 int main() {
-    int nums01[] = {3, 2, 2, 3};
-    int numsSize = sizeof(nums01) / sizeof(nums01[0]);
-    int val = 3;
-    int newSize = removeElement(nums01, numsSize, val);
-    printf("New length: %d\n", newSize);
-
-    int nums02[] = {0,1,2,2,3,0,4,2};
-    numsSize = sizeof(nums02) / sizeof(nums02[0]);
-    val = 2;
-    newSize = removeElement(nums01, numsSize, val);
-    printf("New length: %d\n", newSize);
+   
     return 0;
 }
