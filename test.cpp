20d6750aa16be15655b0e7332ad6a5c7bb61ec18
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-06-28 21:21:14 +0800

    2025.06.28

--- test.cpp
+++ test.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-// 判斷一個數是否為質數
-bool isPrime(int n) {
-    if (n < 2) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    for (int i = 3; i < sqrt(n)+1; i += 2) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
-
 int main() {
-    int a, b;
-    while (cin >> a >> b) {
-        int count = 0;
-        for (int i = a; i <= b; ++i) {
-            if (isPrime(i)) count++;
-        }
-        cout << count << endl;
-    }
+    float f;
+    double df;
+    cout << sizeof(f) << endl; // 4
+    cout << sizeof(df) << endl; // 8
     return 0;
 }
