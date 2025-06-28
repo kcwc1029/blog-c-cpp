bddef84e42bdf68679fe04011d4f7f18a0491bf4
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-06-28 22:41:54 +0800

    2025.06.28

--- test.cpp
+++ test.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-    float f;
-    double df;
-    cout << sizeof(f) << endl; // 4
-    cout << sizeof(df) << endl; // 8
+    int i = 2147483647;  // 最大值（32-bit int）
+
+    cout << "sizeof(i) = " << sizeof(i) << " bytes" << endl;
+    cout << "原始值：i = " << i << endl; // 2147483647
+
+    i++;  // 溢位
+    cout << "i++ 後：i = " << i << endl; // -2147483648
     return 0;
 }
