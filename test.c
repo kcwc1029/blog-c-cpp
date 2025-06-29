ba89516029e9ad3939f22fea3fb2760528fe3185
Wei-Cheng Chen <N96144250@gs.ncku.edu.tw>
2025-06-29 21:25:26 +0800

    2025.06.29

--- test.c
+++ test.c
@@ -1,52 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#define ARRAYSIZE 5
+#include <iostream>
+// #include <iomanip>  // 為了使用 hex 輸出
+using namespace std;
 
-// 基本node
-struct listnode {
-    int data;
-    struct listnode *next;
-};
-typedef struct listnode ListNode;
-
-// 新增node
-ListNode *genListNode(int data,ListNode *next){
-    ListNode *node = (ListNode *)malloc(sizeof(ListNode));
-    assert(node != NULL);
-    node->data = data;
-    node->next = next;
-    return node;
-}
-
-
-// 印出每一個節點
-void printLinkedList(ListNode *node){
-    for (; node != NULL; node = node->next)
-        printf("data = %d\n", node->data);
-}
-
-// 移除mode
-void freeLinkedList(ListNode *node){
-    while (node != NULL) {
-        ListNode *next = node->next;
-        free(node);
-        node = next;
-    }
-}
-
-// main
-int main(void){
-    int array[5] = {1,2,3,4,5};
-    ListNode *head; // 整條 Linked List 的起點
-    ListNode *previous = NULL; // 目前最新插入的節點
-    for (int i = 0; i < ARRAYSIZE; i++) {
-        // 要把「新節點的 next 指向 previous」，然後更新 previous 成新節點。
-        head = genListNode(array[i], previous); // 整個linklist最新的頭
-        previous = head;// 當前linklist最新的頭
-    }
-    // 做出來linklist
-    printLinkedList(head);
-    freeLinkedList(head);
+int main() {
+    char c = 'a';
+    cout << c << endl; // a
+    // cout << static_cast<int>(c) << endl; // 97
+    // cout << hex << static_cast<int>(c) << endl; // 0x61
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// 宣告 inline 函式
-inline int square(int x) {
-    return x * x;
-}
-
 int main() {
-    int n = 5;
-    int result = square(n);  // 這裡會被展開成：n * n
-    cout << "平方是：" << result << endl;
+    string s = "";
+    cin >> s;
+    cout << "hello, " << s << endl;
     return 0;
 }
+
